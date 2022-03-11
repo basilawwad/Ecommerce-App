3bba7ac5b1511e6a587d325ecc5e86f98c2adaae
basil <bawwad032@gmail.com>
2022-03-11 06:08:46 +0300

    Router added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
 
 import Styles from './Navbar.module.css';
 
 const Navbar = ({ totalItems }) => {
+  const location = useLocation();
+
   return (
     <AppBar position='sticky' className={Styles.appBar} color='inherit'>
       <Toolbar className={Styles.toolBar}>
-        <Typography variant='h6' className={Styles.title} color='inherit'>
+        <Typography
+          variant='h6'
+          type='button'
+          className={Styles.title}
+          color='inherit'
+          component={Link}
+          to='/'>
           {/* <img src={} alt='commerceApp' height="25px" className={Styles.image} /> */}
           Test App
         </Typography>
         <div className={Styles.grow}></div>
-        <div className={Styles.button}>
-          <IconButton aria-label='Show cart items' color='inherit'>
-            <Badge badgeContent={totalItems} color='secondary'>
-              <ShoppingCart />
-            </Badge>
-          </IconButton>
-        </div>
+        {location.pathname == '/' ? (
+          <div className={Styles.button}>
+            <IconButton aria-label='Show cart items' color='inherit' component={Link} to='/Cart'>
+              <Badge badgeContent={totalItems} color='secondary'>
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        ) : (
+          ''
+        )}
       </Toolbar>
     </AppBar>
   );
